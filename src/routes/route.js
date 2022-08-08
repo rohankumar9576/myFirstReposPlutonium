bea6948e1fa0bd7ce407531d0538dcc15132f16a
rohankumar9576 <you@example.com>
2022-08-09 01:26:43 +0530

    first half assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,18 +9,31 @@ const router = express.Router();
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
-    res.send('My second ever api!')
+    res.send('My second ever api!') 
+
+          // Question no. 4    month name
     let arr=["jan","feb","march","apr","may","june","july","aug","sep","oct","nov","dec"]
     const resultArr=_.chunk(  arr,3) 
     console.log(  "month name",resultArr)
+
+
+
+       let oddNUM=[1,3,5,7,9,11,13,15,17,19]
+       const tailarr=_.tail(oddNUM)
+       console.log(tailarr)
+          
+
+       // next problem
+
     let arr1=[1,2,3,4];
     let arr2=[4,2,6,5];
     let arr3=[8,7,9,10];
     let arr4=[8,14,12,13]
-
     const unresult=_.union(arr1,arr2,arr3,arr4)
     console.log("union of number",unresult)
     
+    //   problem 5
+
     let pairarr=[
 
         ['name', 'Rohan'], 
@@ -28,11 +41,10 @@ router.get('/test-me', function (req, res) {
         ['used', 'FunctionUp']
 
     ]
-    
+   
 const pairResult=dash.fromPairs(pairarr)
 console.log(pairResult)
- let dubarr=[1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,,17]
-    
+  
 
 });
 

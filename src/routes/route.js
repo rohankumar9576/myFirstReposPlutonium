a37f7c38bdefac956397b2eef465c44e9f2172d3
rohankumar9576 <you@example.com>
2022-08-08 16:31:50 +0530

    new codes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const _=require("underscore")
+const dash=require("lodash")
+
+// const myun=require("union")
 const abc = require('../introduction/intro')
 const router = express.Router();
 
@@ -6,6 +10,30 @@ router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
     res.send('My second ever api!')
+    let arr=["jan","feb","march","apr","may","june","july","aug","sep","oct","nov","dec"]
+    const resultArr=_.chunk(  arr,3) 
+    console.log(  "month name",resultArr)
+    let arr1=[1,2,3,4];
+    let arr2=[4,2,6,5];
+    let arr3=[8,7,9,10];
+    let arr4=[8,14,12,13]
+
+    const unresult=_.union(arr1,arr2,arr3,arr4)
+    console.log("union of number",unresult)
+    
+    let pairarr=[
+
+        ['name', 'Rohan'], 
+        ['live', 'Bihar'], 
+        ['used', 'FunctionUp']
+
+    ]
+    
+const pairResult=dash.fromPairs(pairarr)
+console.log(pairResult)
+ let dubarr=[1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,,17]
+    
+
 });
 
 

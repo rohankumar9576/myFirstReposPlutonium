67a5b028f74159ada13a83a76049e83f0abb3caa
rohankumar9576 <you@example.com>
2022-08-09 02:11:26 +0530

    2nd half assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,3 +1,6 @@
+
+
+
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
@@ -10,29 +13,94 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
+
 router.get('/students', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
-})
+})    
+       //QUESTION NO. 1
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+router.get('/movie',function(req ,res){
 
-    let requestParams = req.params
+    let movie=['rajababu','koyla,nayak','krish', 'zurm']
+    res.send(movie)
+})  
 
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
+//          QUESTION NO.2    AND 3
+
+router.get('/movie/:indexNuber',function(req,res){
+
+    let movie=['rajababu','koyla','nayak','krish', 'zurm']
+
+
+let requestParams=req.params.indexNuber
+
+if(requestParams<movie.length){
+
+console.log("movie name is ",movie[requestParams])
+}
+else{
+console.log("invalid input hai")
+    res.send("invalid input")
     
-    res.send('Dummy response')
+}
+res.send("movie is available go and watch")
+
 })
 
-module.exports = router;
+   //QUESTION NO 4
+
+router.get('/films',function(req,res){
+    let films=
+
+    [ {
+    "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+res.send(films)
+console.log(films)
+})
+
+           
+//  QUESTION NO.5
+router.get('/films/:filmId', function(req,res){
+    let id=req.params.filmId
+
+    let films=
+    [ {
+    "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+       for(let i=0;i <films.length;i++){
+
+        if(id==i+1){
+
+            console.log(films[i])
+        
+        }
+     }
+       
+    }
+
+
+)

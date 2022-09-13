b9e343157b551b7d1b7cf196fbd4c216bfebb6f0
rohankumar9576 <rohanraj9576@gmail.com>
2022-09-14 00:12:09 +0530

    Blogging Site Mini Project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-
 const route = require('./routes/route.js');
 const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb+srv://project-1-G54:prlPkw7J8MyCL0ZY@project-1-g54.fuj1gxz.mongodb.net/Project-1", {
     useNewUrlParser: true
@@ -15,9 +13,7 @@ mongoose.connect("mongodb+srv://project-1-G54:prlPkw7J8MyCL0ZY@project-1-g54.fuj
     .then(() => { console.log("MongoDB is connected") })
     .catch(err => console.log(err))
 
-
 app.use('/', route);
-
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
